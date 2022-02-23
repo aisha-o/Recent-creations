a3b1956f7a09de9d9e4a3de428bbf8feba9aba9f
A'isha Onisabi <98278279+aisha-o@users.noreply.github.com>
2022-02-23 15:52:27 +0100

    Add files via upload

---
+++ project.js
@@ -0,0 +1,19 @@
+$(".slides").owlCarousel({
+	margin: 10,
+	loop: true,
+	dots: true,
+	autoplay: true,
+	autoplayTimeout: 3000,
+	autoplayHoverPause: true,
+	responsive: {
+		0: {
+			items: 1,
+		},
+		650: {
+			items: 2,
+		},
+		1000: {
+			items: 3,
+		},
+	},
+});
